ce891d8fef237487620d0f47eefb9cf2f626bb70
ChiragD12 <deswalc@gmail.com>
2026-07-30 16:42:43 +0530

    Track Capacitor public assets

---
+++ ios/App/App/public/assets/bookmark-Dkt5yIv7.js
@@ -0,0 +1,1 @@
+import{I as e}from"./index-vkIIiQCh.js";var t=e(`circle-x`,[[`circle`,{cx:`12`,cy:`12`,r:`10`,key:`1mglay`}],[`path`,{d:`m15 9-6 6`,key:`1uzhvr`}],[`path`,{d:`m9 9 6 6`,key:`z0biqf`}]]),n=e(`bookmark`,[[`path`,{d:`M17 3a2 2 0 0 1 2 2v15a1 1 0 0 1-1.496.868l-4.512-2.578a2 2 0 0 0-1.984 0l-4.512 2.578A1 1 0 0 1 5 20V5a2 2 0 0 1 2-2z`,key:`oz39mx`}]]);export{t as n,n as t};
