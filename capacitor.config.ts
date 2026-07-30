13e3efc53512069a71ee0fb3ba15cbf71123f6ac
ChiragD12 <deswalc@gmail.com>
2026-07-30 12:13:17 +0530

    Initialize Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.chirag.visionquizcraft',
+  appName: 'Vision Quiz Craft',
+  webDir: 'dist'
+};
+
+export default config;
